f11e5d5b450c84237c4f8a8c1ebded3c9827d852
Jianbang Yang <yangjianbang112@gmail.com>
2024-06-12 20:37:47 +0800

    fix compile error

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -160,6 +160,8 @@ if(NOT WIN32)
       -Wno-error=array-bounds #Warning in Eigen, gcc 12.2
       -Wno-error=ignored-attributes # Warnings in Eigen, gcc 6.3
       -Wno-error=int-in-bool-context # Warning in Eigen gcc 7.2
+      -Wno-error=strict-aliasing
+      -Wno-error=sign-compare
       -Wimplicit-fallthrough=0 # Warning in tinyformat.h
       ${fsanitize})
 
